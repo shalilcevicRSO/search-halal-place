c22c90c3819c25a913fcb2d337c59a36de8f9b94
selmaHalilcevic <selmahalilcevicius@gmail.com>
2021-01-04 20:55:33 +0100

    adding lib module

---
+++ lib/src/main/java/com/selma/halal/food/project/lib/SearchHalalPlaceMetadata.java
@@ -0,0 +1,42 @@
+package com.selma.halal.food.project.lib;
+
+
+public class SearchHalalPlaceMetadata {
+
+    private Integer id;
+    private String name;
+    private String city;
+    private String country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
