38a18f2aa4b33ae6115e0082eae22d3b17c70c74
selmaHalilcevic <selmahalilcevicius@gmail.com>
2021-01-04 21:36:40 +0100

    fixing sql script

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,1 +1,1 @@
-INSERT INTO search_place_metadata (name, city, country) VALUES ('Butchery Šišić', 'Sarajevo', 'Bosnia and Herzegovina');
+INSERT INTO search_place_metadata (id, name, city, country) VALUES (1, 'Butchery Sisic', 'Sarajevo', 'Bosnia and Herzegovina');
