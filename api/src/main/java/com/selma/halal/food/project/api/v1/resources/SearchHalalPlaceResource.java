b395a8fb21663c4d0370417de2cf62f510943024
selmaHalilcevic <selmahalilcevicius@gmail.com>
2021-01-17 23:05:40 +0100

    fixing k8s and connecting to halal place catalog service

--- api/src/main/java/com/selma/halal/food/project/api/v1/resources/SearchHalalPlaceResource.java
+++ api/src/main/java/com/selma/halal/food/project/api/v1/resources/SearchHalalPlaceResource.java
@@ -1,8 +1,13 @@
 package com.selma.halal.food.project.api.v1.resources;
 
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.selma.halal.food.project.lib.SearchHalalPlaceMetadata;
 import com.selma.halal.food.project.services.beans.SearchPlaceBean;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -50,18 +55,22 @@ public class SearchHalalPlaceResource {
         return Response.status(Response.Status.OK).entity(searchHalalPlaceMetadata).build();
     }
 
-//    @GET
-//    @Path("/{city}")
-//    public Response getSearchHalalPlaceMetadata(@PathParam("city") String city) {
-//
-//        SearchHalalPlaceMetadata searchHalalPlaceMetadata = searchPlaceBean.getSearchHalalPlaceMetadata(city);
-//
-//        if (searchHalalPlaceMetadata == null) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//
-//        return Response.status(Response.Status.OK).entity(searchHalalPlaceMetadata).build();
-//    }
+    // gets data from halal-place-catalog
+    @GET
+    @Path("/{city}/{country}")
+    public String getSearchHalalPlaceMetadata(
+            @PathParam("city") String city,
+            @PathParam("country") String country) {
+
+        String halalPlaceMetadata = searchFromHalalPlaceCatalogService(city, country);
+
+        if (halalPlaceMetadata == null) {
+            throw new NotFoundException();
+        }
+
+        return halalPlaceMetadata;
+    }
+
 
     @POST
     public Response createSearchHalalPlaceMetadata(SearchHalalPlaceMetadata searchHalalPlaceMetadata) {
@@ -74,6 +83,7 @@ public class SearchHalalPlaceResource {
         }
         else {
             searchHalalPlaceMetadata = searchPlaceBean.createSearchHalalPlaceMetadata(searchHalalPlaceMetadata);
+
         }
 
         return Response.status(Response.Status.OK).entity(searchHalalPlaceMetadata).build();
@@ -106,6 +116,21 @@ public class SearchHalalPlaceResource {
         else {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+    }
+
+    // Connects to the halal-place-catalog service
+    private String searchFromHalalPlaceCatalogService(String city, String country) {
+        HttpResponse<String> response = null;
+        try{
+            Unirest.setTimeouts(0, 0);
+            response =  Unirest.get("http://localhost:8080/v1/places/" + city + "/" + country)
+                    .asString();
+
+        }
+        catch (UnirestException e) {
+            e.printStackTrace();
+        }
+        return response.getBody();
     }
 
 
